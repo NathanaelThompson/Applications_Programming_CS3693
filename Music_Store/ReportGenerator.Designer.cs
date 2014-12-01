e7018313cc9b2253e3e64ca6bcac5cd2dc6bf111
TheNamedOne <nathanael.thompson90@gmail.com>
2014-12-01 18:43:46 -0500

    Dummy report email
    Sends a placeholder html file to the email of your choice.

--- Music_Store/ReportGenerator.Designer.cs
+++ Music_Store/ReportGenerator.Designer.cs
@@ -39,13 +39,13 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.gvReports = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.rbGrossSales = new System.Windows.Forms.RadioButton();
-            this.rbMonthlySales = new System.Windows.Forms.RadioButton();
-            this.rbEmployees = new System.Windows.Forms.RadioButton();
-            this.rbMostPopGenre = new System.Windows.Forms.RadioButton();
-            this.rbMostPopArtist = new System.Windows.Forms.RadioButton();
-            this.rbMostPopAlbum = new System.Windows.Forms.RadioButton();
             this.rbCustomers = new System.Windows.Forms.RadioButton();
+            this.rbMostPopAlbum = new System.Windows.Forms.RadioButton();
+            this.rbMostPopArtist = new System.Windows.Forms.RadioButton();
+            this.rbMostPopGenre = new System.Windows.Forms.RadioButton();
+            this.rbEmployees = new System.Windows.Forms.RadioButton();
+            this.rbMonthlySales = new System.Windows.Forms.RadioButton();
+            this.rbGrossSales = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gvReports)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -54,18 +54,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 34);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(39, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 17);
             this.label1.TabIndex = 0;
             // 
             // generateButton
             // 
-            this.generateButton.Location = new System.Drawing.Point(195, 90);
-            this.generateButton.Margin = new System.Windows.Forms.Padding(2);
+            this.generateButton.Location = new System.Drawing.Point(260, 111);
+            this.generateButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.generateButton.Name = "generateButton";
-            this.generateButton.Size = new System.Drawing.Size(100, 23);
+            this.generateButton.Size = new System.Drawing.Size(133, 28);
             this.generateButton.TabIndex = 8;
             this.generateButton.Text = "Generate Report";
             this.generateButton.UseVisualStyleBackColor = true;
@@ -74,21 +73,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Enabled = false;
-            this.label2.Location = new System.Drawing.Point(64, 193);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(85, 238);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(46, 17);
             this.label2.TabIndex = 9;
             this.label2.Text = "Email:";
             // 
             // emailButton
             // 
-            this.emailButton.Enabled = false;
-            this.emailButton.Location = new System.Drawing.Point(349, 188);
-            this.emailButton.Margin = new System.Windows.Forms.Padding(2);
+            this.emailButton.Location = new System.Drawing.Point(465, 231);
+            this.emailButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.emailButton.Name = "emailButton";
-            this.emailButton.Size = new System.Drawing.Size(100, 23);
+            this.emailButton.Size = new System.Drawing.Size(133, 28);
             this.emailButton.TabIndex = 10;
             this.emailButton.Text = "Email Report";
             this.emailButton.UseVisualStyleBackColor = true;
@@ -96,19 +92,18 @@
             // 
             // emailBox
             // 
-            this.emailBox.Enabled = false;
-            this.emailBox.Location = new System.Drawing.Point(103, 191);
-            this.emailBox.Margin = new System.Windows.Forms.Padding(2);
+            this.emailBox.Location = new System.Drawing.Point(137, 235);
+            this.emailBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.emailBox.Name = "emailBox";
-            this.emailBox.Size = new System.Drawing.Size(242, 20);
+            this.emailBox.Size = new System.Drawing.Size(321, 22);
             this.emailBox.TabIndex = 11;
             // 
             // helpButton
             // 
-            this.helpButton.Location = new System.Drawing.Point(12, 562);
-            this.helpButton.Margin = new System.Windows.Forms.Padding(2);
+            this.helpButton.Location = new System.Drawing.Point(16, 692);
+            this.helpButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.helpButton.Name = "helpButton";
-            this.helpButton.Size = new System.Drawing.Size(70, 25);
+            this.helpButton.Size = new System.Drawing.Size(93, 31);
             this.helpButton.TabIndex = 13;
             this.helpButton.Text = "Help";
             this.helpButton.UseVisualStyleBackColor = true;
@@ -116,10 +111,10 @@
             // 
             // closeButton
             // 
-            this.closeButton.Location = new System.Drawing.Point(424, 562);
-            this.closeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.closeButton.Location = new System.Drawing.Point(565, 692);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(70, 25);
+            this.closeButton.Size = new System.Drawing.Size(93, 31);
             this.closeButton.TabIndex = 14;
             this.closeButton.Text = "Close";
             this.closeButton.UseVisualStyleBackColor = true;
@@ -129,21 +124,23 @@
             // 
             this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(178, 301);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(237, 370);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.Size = new System.Drawing.Size(200, 185);
             this.pictureBox1.TabIndex = 15;
             this.pictureBox1.TabStop = false;
             // 
             // gvReports
             // 
             this.gvReports.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gvReports.Location = new System.Drawing.Point(500, 12);
+            this.gvReports.Location = new System.Drawing.Point(667, 15);
+            this.gvReports.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gvReports.Name = "gvReports";
             this.gvReports.RowHeadersVisible = false;
-            this.gvReports.Size = new System.Drawing.Size(550, 575);
+            this.gvReports.Size = new System.Drawing.Size(733, 708);
             this.gvReports.TabIndex = 16;
+            this.gvReports.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvReports_CellContentClick);
             // 
             // groupBox1
             // 
@@ -155,90 +152,99 @@
             this.groupBox1.Controls.Add(this.rbEmployees);
             this.groupBox1.Controls.Add(this.rbMonthlySales);
             this.groupBox1.Controls.Add(this.rbGrossSales);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(482, 128);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(643, 158);
             this.groupBox1.TabIndex = 17;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "What should be including in the report?";
+            // 
+            // rbCustomers
+            // 
+            this.rbCustomers.AutoSize = true;
+            this.rbCustomers.Location = new System.Drawing.Point(140, 66);
+            this.rbCustomers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbCustomers.Name = "rbCustomers";
+            this.rbCustomers.Size = new System.Drawing.Size(96, 21);
+            this.rbCustomers.TabIndex = 10;
+            this.rbCustomers.Text = "Customers";
+            this.rbCustomers.UseVisualStyleBackColor = true;
+            // 
+            // rbMostPopAlbum
+            // 
+            this.rbMostPopAlbum.AutoSize = true;
+            this.rbMostPopAlbum.Location = new System.Drawing.Point(455, 38);
+            this.rbMostPopAlbum.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbMostPopAlbum.Name = "rbMostPopAlbum";
+            this.rbMostPopAlbum.Size = new System.Drawing.Size(162, 21);
+            this.rbMostPopAlbum.TabIndex = 9;
+            this.rbMostPopAlbum.Text = "Most Popular Albums";
+            this.rbMostPopAlbum.UseVisualStyleBackColor = true;
+            // 
+            // rbMostPopArtist
+            // 
+            this.rbMostPopArtist.AutoSize = true;
+            this.rbMostPopArtist.Location = new System.Drawing.Point(281, 66);
+            this.rbMostPopArtist.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbMostPopArtist.Name = "rbMostPopArtist";
+            this.rbMostPopArtist.Size = new System.Drawing.Size(155, 21);
+            this.rbMostPopArtist.TabIndex = 6;
+            this.rbMostPopArtist.Text = "Most Popular Artists";
+            this.rbMostPopArtist.UseVisualStyleBackColor = true;
+            // 
+            // rbMostPopGenre
+            // 
+            this.rbMostPopGenre.AutoSize = true;
+            this.rbMostPopGenre.Location = new System.Drawing.Point(281, 38);
+            this.rbMostPopGenre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbMostPopGenre.Name = "rbMostPopGenre";
+            this.rbMostPopGenre.Size = new System.Drawing.Size(163, 21);
+            this.rbMostPopGenre.TabIndex = 4;
+            this.rbMostPopGenre.Text = "Most Popular Genres";
+            this.rbMostPopGenre.UseVisualStyleBackColor = true;
+            // 
+            // rbEmployees
+            // 
+            this.rbEmployees.AutoSize = true;
+            this.rbEmployees.Location = new System.Drawing.Point(140, 38);
+            this.rbEmployees.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbEmployees.Name = "rbEmployees";
+            this.rbEmployees.Size = new System.Drawing.Size(130, 21);
+            this.rbEmployees.TabIndex = 2;
+            this.rbEmployees.Text = "Employee Sales";
+            this.rbEmployees.UseVisualStyleBackColor = true;
+            // 
+            // rbMonthlySales
+            // 
+            this.rbMonthlySales.AutoSize = true;
+            this.rbMonthlySales.Location = new System.Drawing.Point(8, 66);
+            this.rbMonthlySales.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbMonthlySales.Name = "rbMonthlySales";
+            this.rbMonthlySales.Size = new System.Drawing.Size(117, 21);
+            this.rbMonthlySales.TabIndex = 1;
+            this.rbMonthlySales.Text = "Monthly Sales";
+            this.rbMonthlySales.UseVisualStyleBackColor = true;
             // 
             // rbGrossSales
             // 
             this.rbGrossSales.AutoSize = true;
             this.rbGrossSales.Checked = true;
-            this.rbGrossSales.Location = new System.Drawing.Point(7, 31);
+            this.rbGrossSales.Location = new System.Drawing.Point(9, 38);
+            this.rbGrossSales.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbGrossSales.Name = "rbGrossSales";
-            this.rbGrossSales.Size = new System.Drawing.Size(81, 17);
+            this.rbGrossSales.Size = new System.Drawing.Size(106, 21);
             this.rbGrossSales.TabIndex = 0;
             this.rbGrossSales.TabStop = true;
             this.rbGrossSales.Text = "Gross Sales";
             this.rbGrossSales.UseVisualStyleBackColor = true;
             // 
-            // rbMonthlySales
-            // 
-            this.rbMonthlySales.AutoSize = true;
-            this.rbMonthlySales.Location = new System.Drawing.Point(6, 54);
-            this.rbMonthlySales.Name = "rbMonthlySales";
-            this.rbMonthlySales.Size = new System.Drawing.Size(91, 17);
-            this.rbMonthlySales.TabIndex = 1;
-            this.rbMonthlySales.Text = "Monthly Sales";
-            this.rbMonthlySales.UseVisualStyleBackColor = true;
-            // 
-            // rbEmployees
-            // 
-            this.rbEmployees.AutoSize = true;
-            this.rbEmployees.Location = new System.Drawing.Point(105, 31);
-            this.rbEmployees.Name = "rbEmployees";
-            this.rbEmployees.Size = new System.Drawing.Size(100, 17);
-            this.rbEmployees.TabIndex = 2;
-            this.rbEmployees.Text = "Employee Sales";
-            this.rbEmployees.UseVisualStyleBackColor = true;
-            // 
-            // rbMostPopGenre
-            // 
-            this.rbMostPopGenre.AutoSize = true;
-            this.rbMostPopGenre.Location = new System.Drawing.Point(211, 31);
-            this.rbMostPopGenre.Name = "rbMostPopGenre";
-            this.rbMostPopGenre.Size = new System.Drawing.Size(124, 17);
-            this.rbMostPopGenre.TabIndex = 4;
-            this.rbMostPopGenre.Text = "Most Popular Genres";
-            this.rbMostPopGenre.UseVisualStyleBackColor = true;
-            // 
-            // rbMostPopArtist
-            // 
-            this.rbMostPopArtist.AutoSize = true;
-            this.rbMostPopArtist.Location = new System.Drawing.Point(211, 54);
-            this.rbMostPopArtist.Name = "rbMostPopArtist";
-            this.rbMostPopArtist.Size = new System.Drawing.Size(118, 17);
-            this.rbMostPopArtist.TabIndex = 6;
-            this.rbMostPopArtist.Text = "Most Popular Artists";
-            this.rbMostPopArtist.UseVisualStyleBackColor = true;
-            // 
-            // rbMostPopAlbum
-            // 
-            this.rbMostPopAlbum.AutoSize = true;
-            this.rbMostPopAlbum.Location = new System.Drawing.Point(341, 31);
-            this.rbMostPopAlbum.Name = "rbMostPopAlbum";
-            this.rbMostPopAlbum.Size = new System.Drawing.Size(124, 17);
-            this.rbMostPopAlbum.TabIndex = 9;
-            this.rbMostPopAlbum.Text = "Most Popular Albums";
-            this.rbMostPopAlbum.UseVisualStyleBackColor = true;
-            // 
-            // rbCustomers
-            // 
-            this.rbCustomers.AutoSize = true;
-            this.rbCustomers.Location = new System.Drawing.Point(105, 54);
-            this.rbCustomers.Name = "rbCustomers";
-            this.rbCustomers.Size = new System.Drawing.Size(74, 17);
-            this.rbCustomers.TabIndex = 10;
-            this.rbCustomers.Text = "Customers";
-            this.rbCustomers.UseVisualStyleBackColor = true;
-            // 
             // ReportGenerator
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1058, 599);
+            this.ClientSize = new System.Drawing.Size(1411, 737);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.gvReports);
             this.Controls.Add(this.pictureBox1);
@@ -249,7 +255,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "ReportGenerator";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- Music_Store/ReportGenerator.cs
+++ Music_Store/ReportGenerator.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
 
 namespace Music_Store
 {
@@ -53,13 +55,60 @@ namespace Music_Store
 
         private void emailButton_Click(object sender, EventArgs e)
         {
+            //Create new mail message and attachment
+            MailMessage mail = new MailMessage();
+            Attachment attachment = new Attachment("placeholderHtmlFile.html");
 
+            string emailToSplit = emailBox.Text;
+            char[] emailSplit = emailToSplit.ToCharArray();
+
+            //check for the characters @ and . in the submitted email address
+            if ((!(emailSplit.Contains<char>('@'))) || (!(emailSplit.Contains<char>('.'))))
+            {
+                MessageBox.Show("Email address not formatted correctly", "Input error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            //Create the message
+            mail.Attachments.Add(attachment);
+            mail.From = new MailAddress("musicstoredonotreply@gmail.com");
+            mail.To.Add(emailBox.Text);
+            mail.Subject = "Music Store Lost Password (Do Not Reply)";
+            mail.Body = "";
+
+            using (SmtpClient client = new SmtpClient())
+            {
+                //setting up the network
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("musicstoredonotreply@gmail.com", "AppProgCS3693");
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                //try to send the message, display message if it fails
+                try
+                {
+                    client.Send(mail);
+                    MessageBox.Show("Report sent to email: " + emailToSplit + " ", "Job complete",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error sending message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
         private void ReportGenerator_FormClosing(object sender, FormClosingEventArgs e)
         {
             this.Hide();
             e.Cancel = true;
+        }
+
+        private void gvReports_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
 
 
